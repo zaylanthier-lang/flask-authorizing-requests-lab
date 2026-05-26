10e55d36576ea963fd964a539da7058143ab46b5
Zayden <zaylanthier-lang>
2026-05-26 17:32:15 -0400

    Finished Code

--- server/app.py
+++ server/app.py
@@ -87,13 +87,35 @@ class CheckSession(Resource):
 class MemberOnlyIndex(Resource):
     
     def get(self):
-        pass
+
+        if not session.get('user_id'):
+            return {'message': 'Unauthorized'}, 401
+
+        articles = Article.query.filter(Article.is_member_only == True).all()
+
+        articles_json = [ArticleSchema().dump(article) for article in articles]
+
+        return make_response(articles_json, 200)
+
 
 class MemberOnlyArticle(Resource):
     
     def get(self, id):
-        pass
 
+        if not session.get('user_id'):
+            return {'message': 'Unauthorized'}, 401
+
+        article = Article.query.filter(
+            Article.id == id,
+            Article.is_member_only == True
+        ).first()
+
+        if not article:
+            return {'message': 'Article not found'}, 404
+
+        article_json = ArticleSchema().dump(article)
+
+        return make_response(article_json, 200)
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
 api.add_resource(ShowArticle, '/articles/<int:id>', endpoint='show_article')
